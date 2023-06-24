ea44449da4a754219161488fdc4b546357294692
Brett Chapin <brett.chapin89@gmail.com>
2023-06-24 17:31:09 -0400

    Added a bunch of convenience middlewares to app.js

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const compression = require('compression');
 
 const app = express();
 
@@ -14,9 +19,32 @@ if (process.env.NODE_ENV === 'development') {
 }
 app.use(express.json());
 
+app.use(helmet());
+
+const limiter = rateLimit({
+  max: 10,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('*', limiter);
+
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoed({ extended: true, limit: '10kb' }));
+
+app.use(mongoSanitize());
+
+app.use(xss());
+
+app.use(compression());
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 // ROUTES
 
-app.use('/api/v1/locations', locationRouter);
+// app.use('/api/v1/locations', locationRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));

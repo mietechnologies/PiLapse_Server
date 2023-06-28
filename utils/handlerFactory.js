c383f63f6ee15610205c07d2399c61b626b8f6f6
Brett Chapin <brett.chapin89@gmail.com>
2023-06-27 23:10:07 -0400

    Added getAll and apiFeatures functionality

--- utils/handlerFactory.js
+++ utils/handlerFactory.js
@@ -1,5 +1,6 @@
 const catchAsync = require('./catchAsync');
 const AppError = require('./appError');
+const APIFeatures = require('./apiFeatures');
 
 exports.getOne = (Model, popOptions) =>
   catchAsync(async (req, res, next) => {
@@ -15,6 +16,29 @@ exports.getOne = (Model, popOptions) =>
       status: 'success',
       data: {
         doc,
+      },
+    });
+  });
+
+exports.getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    let filter = {};
+    if (req.params.tourId) filter = { tour: req.params.tourId };
+
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+
+    const docs = await features.query;
+
+    // SEND RESPONSE
+    res.status(200).json({
+      status: 'success',
+      results: docs.length,
+      data: {
+        docs,
       },
     });
   });

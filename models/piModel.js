ee1a73a0e0d0fa4f8aaaac397aa2dea5d7bcb39a
Brett Chapin <brett.chapin89@gmail.com>
2023-06-27 22:10:16 -0400

    Created piModel
    + models/piModel

---
+++ models/piModel.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+
+const piSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: [true, 'Your Pi must have a name to identify it.'],
+  },
+  description: {
+    type: String,
+  },
+  room: {
+    name: {
+      type: String,
+      required: [true, 'A Pi must have a room.'],
+    },
+    windowDirection: {
+      type: String,
+      enum: ['East', 'West', 'North', 'South'],
+      required: [true, 'A Pi must have a window direction.'],
+    },
+  },
+  location: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true,
+      default: 'Point',
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: [true, 'A Pi must belong to a user.'],
+  },
+});
+
+const Pi = mongoose.model('Pi', piSchema);
+
+module.exports = Pi;
